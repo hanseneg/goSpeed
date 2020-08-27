546034a850dd44590c6f572829fe233f416af50d
Ethan Hansen <hanseneg@hotmail.com>
2020-08-27 17:37:38 -0600

    commit2

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const body = document.getElementsByTagName("body")
+const button = document.getElementById("button")
+const visualCount = document.getElementById("visualCount")
+let count = localStorage.count || 0;
+
+visualCount.textContent = count
+button.addEventListener("click", (e) => {
+    count++
+    visualCount.textContent = count
+})
+
+
+
+
+
